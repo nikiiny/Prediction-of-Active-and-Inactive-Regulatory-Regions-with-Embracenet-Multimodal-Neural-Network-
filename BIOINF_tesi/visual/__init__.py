e754c7e594a0a6d6a9c456e68805155389668846
NicoleFormenti <59973282+nikiiny@users.noreply.github.com>
2021-11-16 21:38:23 +0100

    results complete

--- BIOINF_tesi/visual/__init__.py
+++ BIOINF_tesi/visual/__init__.py
@@ -1,5 +1,7 @@
 from .visual import (plot_label_ratio, get_imbalance_ratio_df, get_baseline_df,
-	plot_scores, print_content_results_dict, get_average_AUPRC_df, Compare_Models_Result, parse_output_for_params_dict)
+	plot_scores, print_content_results_dict, get_average_AUPRC_df, Compare_Models_Result, 
+	parse_output_for_params_dict, get_standard_dev_df, compare_model_overall_performance)
 
 __all__ = ['plot_label_ratio', 'get_imbalance_ratio_df', 'get_baseline_df', 'plot_scores', 
-			'print_content_results_dict', 'get_average_AUPRC_df', 'Compare_Models_Result', 'parse_output_for_params_dict']
+			'print_content_results_dict', 'get_average_AUPRC_df', 'Compare_Models_Result', 
+			'parse_output_for_params_dict', 'get_standard_dev_df', 'compare_model_overall_performance']
--- BIOINF_tesi/visual/visual.py
+++ BIOINF_tesi/visual/visual.py
@@ -15,6 +15,8 @@ import warnings
 from scipy.sparse import coo_matrix
 import gc
 import re
+import os
+
 
 TASKS = ['active_E_vs_inactive_E', 'active_P_vs_inactive_P', 
                         'active_E_vs_active_P', 'inactive_E_vs_inactive_P',
@@ -129,15 +131,14 @@ def plot_scores(cells, models=['FFNN','CNN'], k=3, palette=1):
         for task in results_dict[cell].keys():    
             # store baseline AUPRC
             baseline.append(results_dict[cell][task]['baseline_AUPRC'])
-            for model in results_dict[cell][task].keys():
-                if model in models:
+            for model in models:
                     
-                    AUPRC=np.append(AUPRC, results_dict[cell][task][model]['final_train_AUPRC_scores'])
-                    AUPRC=np.append(AUPRC, results_dict[cell][task][model]['final_test_AUPRC_scores'])
-                    TEST_TRAIN.append(['train']*k), TEST_TRAIN.append(['test']*k) 
-                    MODEL.append([model]*k*2)
-                    TASKS.append([task]*k*2)
-                    CELLS.append([cell]*k*2)
+                AUPRC=np.append(AUPRC, results_dict[cell][task][model]['final_train_AUPRC_scores'])
+                AUPRC=np.append(AUPRC, results_dict[cell][task][model]['final_test_AUPRC_scores'])
+                TEST_TRAIN.append(['train']*k), TEST_TRAIN.append(['test']*k) 
+                MODEL.append([model]*k*2)
+                TASKS.append([task]*k*2)
+                CELLS.append([cell]*k*2)
                     
     MODEL=list(itertools.chain(*MODEL))
     TEST_TRAIN=list(itertools.chain(*TEST_TRAIN))
@@ -184,7 +185,8 @@ def print_content_results_dict(models=['FFNN','CNN','EmbraceNetMultimodal','Conc
 
 
 
-def get_average_AUPRC_df(models=['FFNN','CNN','EmbraceNetMultimodal','ConcatNetMultimodal']):
+def get_average_AUPRC_df(models=['FFNN','CNN','ConcatNetMultimodal','EmbraceNetMultimodal','EmbraceNetMultimodal_augm'],
+    rounding=3):
     
     if isinstance(models,str):
         models = [models]
@@ -193,129 +195,56 @@ def get_average_AUPRC_df(models=['FFNN','CNN','EmbraceNetMultimodal','ConcatNetM
         results_dict = pickle.load(fin)
         results_dict = defaultdict(lambda: defaultdict(dict), results_dict)
     
-    models_df_dict = defaultdict(lambda: defaultdict(list))
-    for model in models:
-        df = pd.DataFrame(columns=TASKS, index=CELL_LINES)
-    
+    cells_df_dict = defaultdict(lambda: defaultdict(list))
+
+    for cell in CELL_LINES:
+        df = pd.DataFrame(columns=TASKS, index=models)
+
         for task in TASKS:
-            for cell in CELL_LINES:
+            for model in models:
                 try:
-                    df.loc[cell][task] = results_dict[cell][task][model]['average_CV_AUPRC']
+                    df.loc[model][task] = np.round(results_dict[cell][task][model]['average_CV_AUPRC'],rounding)
                 except:
-                    df.loc[cell][task] = np.nan
+                    df.loc[model][task] = np.nan
                 
-        models_df_dict[model] = df
+        cells_df_dict[cell] = df
     
-    return models_df_dict
+    return cells_df_dict
+
+
+
+def get_standard_dev_df(models=['FFNN','CNN','ConcatNetMultimodal','EmbraceNetMultimodal','EmbraceNetMultimodal_augm'],
+    rounding=3):
+    
+    if isinstance(models,str):
+        models = [models]
+    
+    with open ('results_dict.pickle', 'rb') as fin:
+        results_dict = pickle.load(fin)
+        results_dict = defaultdict(lambda: defaultdict(dict), results_dict)
+    
+    cells_df_dict = defaultdict(lambda: defaultdict(list))
+
+    for cell in CELL_LINES:
+        df = pd.DataFrame(columns=TASKS, index=models)
+
+        for task in TASKS:
+            for model in models:
+                l=[]
+
+                for i in range(1,4):
+                    l.append(results_dict[cell][task][model][f'iteration_n_{i}']['AUPRC_test'][-1]
+                        )
+
+                df.loc[model][task] = np.round(np.std(l), rounding)
+                
+        cells_df_dict[cell] = df
+    
+    return cells_df_dict
 
 
 def dd():
     return defaultdict(dict)
-
-
-class Compare_Models_Result_():
-    
-    def __init__(self):
-        self.models_dict = {'EmbraceNetMultimodal': EmbraceNetMultimodal_NoTrain,
-                   'EmbraceNetMultimodal_augmentation': EmbraceNetMultimodal_NoTrain,
-                  'ConcatNetMultimodal': ConcatNetMultimodal_NoTrain,
-                  'FFNN': FFNN_NoTrain,
-                  'CNN': CNN_NoTrain}
-
-        self.pval_dict = defaultdict(dd)
-
-        warnings.filterwarnings("ignore")
-    
-    
-    def get_model_predictions(self, cell_line, task, model, device):
-
-        model_ = self.models_dict[model]
-        if model == 'CNN':
-            model_ = model_(cell_line, task, device)
-        else:
-            if model.endswith('augmentation'):
-                model_ = model_(cell_line, task, self.X_1.loc[0].shape[1], device=device, augmentation=True)
-            else:
-                model_ = model_(cell_line, task, self.X_1.loc[0].shape[1], device=device)
-
-        state_dict = torch.load(f'models/{cell_line}_{task}_{model}_TEST.pt', map_location=device)
-                    
-        model_.load_state_dict(state_dict['model_state_dict'])
-        model_.double().to(device)
-
-        for p in model_.parameters():
-            p.require_grads = False
-
-        model_.eval()
-
-        with torch.no_grad():
-            if model.startswith(UNIMODAL_NETWORKS_NOSEQ):
-                output = torch.tensor([ model_( self.X_1.loc[i] )[1] for i in range(self.X_1.shape[0]) ])
-            elif model.startswith(UNIMODAL_NETWORKS_SEQ):
-                output = torch.tensor([ model_( self.X_2.loc[i] )[1] for i in range(self.X_2.shape[0]) ])
-            elif model.startswith(MULTIMODAL_NETWORKS):
-                output = torch.tensor([ model_(
-                                            [self.X_1.loc[i],
-                                             self.X_2.loc[i]]
-                                    )[1] for i in range(self.X_1.shape[0]) ])
-        return output
-
-
-    def __call__(self, device, base_model='EmbraceNetMultimodal', 
-                 comparison_models=['FFNN','CNN','ConcatNetMultimodal'],
-                 augmentation_base_model=True):
-    
-        if isinstance(base_model, str):
-            base_model = [base_model]
-        if isinstance(comparison_models, str):
-            comparison_models = [comparison_models]
-
-        MODELS = comparison_models + base_model
-        if augmentation_base_model:
-            MODELS += [f'{base_model[0]}_augmentation']
-            base_model += [f'{base_model[0]}_augmentation']
-
-        for task in tqdm(TASKS, desc='Iter tasks'):
-            pipe_data_load = Build_DataLoader_Pipeline(path_name=f'{task}.pickle')
-            data_class = pipe_data_load.data_class
-
-            for cell_line in tqdm(CELL_LINES, desc='Iter cell lines'):
-
-                self.pval_dict[task][cell_line]= defaultdict(dd)
-                _, self.X_1, _ = data_class.return_index_data_for_cv(cell_line=cell_line, sequence=False)
-                _, self.X_2, _ = data_class.return_index_data_for_cv(cell_line=cell_line, sequence=True)
-
-                self.X_1 =  self.X_1.apply(lambda x: torch.reshape(torch.tensor(x), (1,-1)), axis=1) 
-                self.X_2 =  self.X_2.apply(lambda x: torch.reshape(torch.tensor(process_sequence(x)), (1,4,256)) ) 
-
-                for b_model in tqdm(base_model, desc='Iter models'):
-                    print(b_model)
-
-                    base_pred = self.get_model_predictions(cell_line, task, b_model, device)
-                    for c_model in tqdm(comparison_models + base_model):
-                        print(c_model)
-                        if c_model == b_model:
-                            continue
-                        else:
-                            c_pred = self.get_model_predictions(cell_line, task, c_model, device)
-                        pval = wilcoxon (base_pred, c_pred)
-
-                        del c_pred
-
-                        self.pval_dict[task][cell_line][b_model][c_model] = pval
-                    del base_pred
-
-                del self.X_1
-                del self.X_2 
-
-                with open ('pval_results_dict.pickle', 'wb') as fout:
-                    pickle.dump(OrderedDict(self.pval_dict), fout)
-
-
-            del pipe_data_load 
-            gc.collect()
-
-        return self.pval_dict
 
 
 class Compare_Models_Result():
@@ -331,7 +260,7 @@ class Compare_Models_Result():
         warnings.filterwarnings("ignore")
     
     
-    def get_model_predictions(self, cell_line, task, model, n_iteration):
+    def get_model_predictions(self, cell_line, task, model, n_iteration, device):
 
         model_ = self.models_dict[model]
         if model == 'CNN':
@@ -366,23 +295,34 @@ class Compare_Models_Result():
         return output
 
 
-    def get_model_difference(self):
+    def print_model_difference(self, p_val=0.05):
 
+        self.counter_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0))))
+    
         for task in self.pval_dict.keys():
             for cell_line in self.pval_dict[task].keys():
-                for b_model in self.pval_dict[task][cell_line].keys():
-                    for c_model in self.pval_dict[task][cell_line][b_model].keys():
-                        counter = 0
+                for fold in self.pval_dict[task][cell_line].keys():
+                    for b_model in self.pval_dict[task][cell_line][fold].keys():
+                        for c_model in self.pval_dict[task][cell_line][fold][b_model].keys():
 
-                        for fold in self.pval_dict[task][cell_line][b_model][c_model].keys():
-                            if self.pval_dict[task][cell_line][b_model][c_model][fold] >=0.05:
-                                self.pval_dict[task][cell_line][b_model][c_model][fold]['different']=False
+                            if self.pval_dict[task][cell_line][fold][b_model][c_model] >=p_val:
+                                self.counter_dict[task][cell_line][b_model][c_model]+=0
                             else:
-                                self.pval_dict[task][cell_line][b_model][c_model][fold]['different']=True
-                                counter +=1
+                                self.counter_dict[task][cell_line][b_model][c_model]+=1
+                
+        
+        for task in self.counter_dict.keys():
+            print(f'\n\n================ TASK: {task} ================')
+            for cell_line in self.counter_dict[task].keys():
+                print(f'\n\n{cell_line}')
+                for b_model in self.counter_dict[task][cell_line].keys():
+                    print(f'\n\nBASE MODEL: {b_model}\n')
+                    for c_model in self.counter_dict[task][cell_line][b_model].keys():
 
-                        if counter >=2:
-                            self.pval_dict[task][cell_line][b_model][c_model]['different']=True
+                        if self.counter_dict[task][cell_line][b_model][c_model] >=2:
+                            print(f'{c_model} ===> different: True')
+                        else:
+                            print(f'{c_model} ===> different: False')
 
 
 
@@ -399,6 +339,11 @@ class Compare_Models_Result():
                 base_model = [base_model]
             if isinstance(comparison_models, str):
                 comparison_models = [comparison_models]
+            if isinstance(tasks, str):
+                tasks = [tasks]
+            if isinstance(cell_lines, str):
+                cell_lines = [cell_lines]
+
 
             MODELS = comparison_models + base_model
             if augmentation_base_model:
@@ -406,6 +351,12 @@ class Compare_Models_Result():
                 base_model += [f'{base_model[0]}_augmentation']
 
             for task in tqdm(tasks, desc='Iter tasks'):
+
+                if os.path.exists(f'pval_results_dict_{task}.pickle'):
+                    with open (f'pval_results_dict_{task}.pickle', 'rb') as fin:
+                        self.pval_dict = pickle.load(fin)
+                        self.pval_dict = defaultdict(lambda: defaultdict(dict), self.pval_dict)
+
                 pipe_data_load = Build_DataLoader_Pipeline(path_name=f'{task}.pickle')
                 data_class = pipe_data_load.data_class
 
@@ -413,37 +364,42 @@ class Compare_Models_Result():
                     _, self.X_1, _ = data_class.return_index_data_for_cv(cell_line=cell_line, sequence=False)
                     _, self.X_2, _ = data_class.return_index_data_for_cv(cell_line=cell_line, sequence=True)
 
+                    self.X_1.reset_index(drop=True, inplace=True)
+                    self.X_2.reset_index(drop=True, inplace=True)
+
                     self.X_1 = self.X_1.apply(lambda x: torch.reshape(torch.tensor(x), (1,-1)), axis=1) 
                     self.X_2 = self.X_2.apply(lambda x: torch.reshape(torch.tensor(process_sequence(x)), (1,4,256)) )
 
+
                     for i in tqdm(range(n_folds), desc='Iter folds'):
                         i+=1
+                        self.pval_dict[task][cell_line][str(i)] = defaultdict(dd)
+                        
                         for b_model in tqdm(base_model, desc='iter models'):
-                            self.pval_dict[task][cell_line][b_model] = defaultdict(dd)
-
                             base_pred = self.get_model_predictions(cell_line, task, b_model, i, device)
-                            for c_model in comparison_models:
+
+                            for c_model in MODELS:
                                 if c_model == b_model:
                                     continue
-                                else:
+                                else:    
                                     c_pred = self.get_model_predictions(cell_line, task, c_model, i, device)
-                                pval = wilcoxon (base_pred, c_pred)[1]
+                                    pval = wilcoxon (base_pred, c_pred)[1]
 
-                                del c_pred
-                                self.pval_dict[task][cell_line][b_model][c_model][i] = pval
+                                    del c_pred
+                                    self.pval_dict[task][cell_line][str(i)][b_model][c_model] = pval
 
                             del base_pred
 
                     del self.X_1
                     del self.X_2 
                 
-                    with open ('pval_results_dict.pickle', 'wb') as fout:
+                    with open (f'pval_results_dict_{task}.pickle', 'wb') as fout:
                         pickle.dump(OrderedDict(self.pval_dict), fout)
 
                 del pipe_data_load 
                 #gc.collect()
 
-        self.get_model_difference()
+        self.print_model_difference()
 
         return self.pval_dict
 
@@ -496,3 +452,64 @@ def parse_output_for_params_dict(output, cell_line, task, model, device, verbose
         else:
             torch.save(NN_dict, f'{cell_line}_{model.__name__}_{task}_{i}_test_.pt')
 
+
+def compare_model_overall_performance(base_model = ['EmbraceNetMultimodal','EmbraceNetMultimodal_augm'],
+    compare_model = ['FFNN','CNN','ConcatNetMultimodal']):
+
+
+    with open ('results_dict.pickle', 'rb') as fin:
+        results_dict = pickle.load(fin)
+        results_dict = defaultdict(lambda: defaultdict(dict), results_dict)
+
+
+    BASE_MODEL = base_model
+    COMPARE_MODEL = compare_model
+
+    df_2sided = pd.DataFrame(columns=BASE_MODEL, index=COMPARE_MODEL+BASE_MODEL)
+    df_1sided_greater = pd.DataFrame(columns=BASE_MODEL, index=COMPARE_MODEL+BASE_MODEL)
+    df_1sided_less = pd.DataFrame(columns=BASE_MODEL, index=COMPARE_MODEL+BASE_MODEL)
+
+    for b_model in BASE_MODEL:
+        b_model_score = []
+        
+        for cell in CELL_LINES:
+            for task in TASKS:
+                b_model_score.append( results_dict[cell][task][b_model]['final_test_AUPRC_scores'] )
+        
+        for c_model in COMPARE_MODEL+BASE_MODEL:
+            if c_model == b_model:
+                continue
+                
+            c_model_score = []
+
+            for cell in CELL_LINES:
+                for task in TASKS:
+                    c_model_score.append( results_dict[cell][task][c_model]['final_test_AUPRC_scores'] )
+                
+
+            df_2sided.loc[c_model][b_model] = np.round(
+                    wilcoxon(
+                        list(itertools.chain(*b_model_score)),
+                        list(itertools.chain(*c_model_score))
+                    )[1], 3)
+                
+            
+            df_1sided_greater.loc[c_model][b_model] = np.round(
+                wilcoxon(
+                    list(itertools.chain(*b_model_score)),
+                    list(itertools.chain(*c_model_score)),
+                    alternative='greater'
+                )[1], 3)
+            
+            # c_model median greater than b_model median
+            
+            df_1sided_less.loc[c_model][b_model] = np.round(
+                wilcoxon(
+                    list(itertools.chain(*b_model_score)),
+                    list(itertools.chain(*c_model_score)),
+                    alternative='less'
+                )[1], 3)
+            
+            # c_model median lower than b_model median
+
+    return df_2sided, df_1sided_greater, df_1sided_less
